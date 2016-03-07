766c900b5f05d937427bf2f97cc4de4df5db4b17
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-08 00:55:48 +0100

    write tests for invalid data

--- test.js
+++ test.js
@@ -4,5 +4,14 @@ import saveToDeps from './index';
 test('should saveToDeps', (t) =>
   t.same(saveToDeps('unicorns'), 'unicorns'));
 
-test('should saveToDeps invalid input', (t) =>
-  t.same(saveToDeps(), undefined));
+test('should saveToDeps empty input', (t) =>
+  t.throws(saveToDeps(), /Input should not be empty/));
+
+test('should saveToDeps invalid depsObject', (t) =>
+  t.throws(saveToDeps('', []), /depsObject should be Object/));
+
+test('should saveToDeps invalid depsList', (t) =>
+  t.throws(saveToDeps({}, ''), /depsList should be Array/));
+
+test('should saveToDeps invalid depsList[String]', (t) =>
+  t.throws(saveToDeps({}, [1, 2]), /depsList should be Array\[String\]/));
