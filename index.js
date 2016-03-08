d5f29d37e5f4a2b4d6a7a7619ba91b7b29a44810
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-08 11:56:11 +0100

    complete testcase demo

--- index.js
+++ index.js
@@ -12,22 +12,8 @@ const depsArrayToObjectReducer = (state, dep) =>
 
 // saveToDeps :: Object -> Array[String] -> Object
 const saveToDeps = (depsObject, depsList) => {
-  if (R.and(R.isNil(depsObject), R.isNil(depsList))) {
-    return Promise.reject('Input should not be empty');
-  }
-  if (R.not(R.is(Object, depsObject))) {
-    return Promise.reject('depsObject should be Object');
-  }
-  if (R.not(R.is(Array, depsList))) {
-    return Promise.reject('depsList should be Array');
-  }
-  if (R.not(R.all(R.is(String), depsList))) {
-    return Promise.reject('depsList should be Array[String]');
-  }
-
   const res = R.reduce(depsArrayToObjectReducer, {}, depsList);
   return Promise.resolve(sorted(res));
-  // return new Promise().resolve(sorted({ meow: '^1.0.0'}));
 };
 
 export default saveToDeps;
--- test.js
+++ test.js
@@ -1,24 +1,23 @@
 import test from 'ava';
 import saveToDeps from './index';
+import { deepEqual } from 'assert';
 
-test.only('basic', (t) => saveToDeps({}, ['meow@^1.0.0'])
+test('basic assert.deepEqual', () => saveToDeps({}, ['a@^1.0.0'])
   .then(res => {
-    // t.same(res, { meow: '^1.0.0' })
-    t.same(res, { meow: '^1.0.0' })
+    deepEqual(res, { a: '^1.0.0' });
   }));
 
+test('sorted assert.deepEqual', () => saveToDeps({}, ['b@1.0.0', 'a@1.0.0'])
+  .then(res => {
+    deepEqual(res, { a: '1.0.0', b: '1.0.0' });
+  }));
 
-test('sorted', (t) =>
-  t.same(saveToDeps({}, ['b@1.0.0', 'a@1.0.0']), { a: '1.0.0', b: '1.0.0' }));
+test('basic t.same', t => saveToDeps({}, ['meow@^1.0.0'])
+  .then(res => {
+    t.same(res, { meow: '^1.0.0' });
+  }));
 
-test('empty input', (t) =>
-  t.throws(saveToDeps(), /Input should not be empty/));
-
-test('invalid depsObject', (t) =>
-  t.throws(saveToDeps('', []), /depsObject should be Object/));
-
-test('invalid depsList', (t) =>
-  t.throws(saveToDeps({}, ''), /depsList should be Array/));
-
-test('invalid depsList[String]', (t) =>
-  t.throws(saveToDeps({}, [1, 2]), /depsList should be Array\[String\]/));
+test('sorted t.same', t => saveToDeps({}, ['b@1.0.0', 'a@1.0.0'])
+  .then(res => {
+    t.same(res, { a: '1.0.0', b: '1.0.0' });
+  }));
