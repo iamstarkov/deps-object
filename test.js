c56b04ea98cc148e5a32fe9f710deb00942f1e1e
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-08 01:02:07 +0100

    add more tests

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
 import test from 'ava';
 import saveToDeps from './index';
 
-test('should saveToDeps', (t) =>
-  t.same(saveToDeps('unicorns'), 'unicorns'));
+test('basic', (t) =>
+  t.same(saveToDeps({}, ['meow@^1.0.0']), { meow: '^1.0.0' }));
 
-test('should saveToDeps empty input', (t) =>
+test('sorted', (t) =>
+  t.same(saveToDeps({}, ['b@1.0.0', 'a@1.0.0']), { a: '1.0.0', b: '1.0.0' }));
+
+test('empty input', (t) =>
   t.throws(saveToDeps(), /Input should not be empty/));
 
-test('should saveToDeps invalid depsObject', (t) =>
+test('invalid depsObject', (t) =>
   t.throws(saveToDeps('', []), /depsObject should be Object/));
 
-test('should saveToDeps invalid depsList', (t) =>
+test('invalid depsList', (t) =>
   t.throws(saveToDeps({}, ''), /depsList should be Array/));
 
-test('should saveToDeps invalid depsList[String]', (t) =>
+test('invalid depsList[String]', (t) =>
   t.throws(saveToDeps({}, [1, 2]), /depsList should be Array\[String\]/));
