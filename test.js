27c7c24a8efebd5b2c8fb97b99da20bde3617190
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-08 11:59:09 +0100

    rearrange testcases

--- test.js
+++ test.js
@@ -2,22 +2,22 @@ import test from 'ava';
 import saveToDeps from './index';
 import { deepEqual } from 'assert';
 
-test('basic assert.deepEqual', () => saveToDeps({}, ['a@^1.0.0'])
-  .then(res => {
-    deepEqual(res, { a: '^1.0.0' });
-  }));
-
-test('sorted assert.deepEqual', () => saveToDeps({}, ['b@1.0.0', 'a@1.0.0'])
-  .then(res => {
-    deepEqual(res, { a: '1.0.0', b: '1.0.0' });
-  }));
-
 test('basic t.same', t => saveToDeps({}, ['meow@^1.0.0'])
   .then(res => {
     t.same(res, { meow: '^1.0.0' });
   }));
 
+test('basic assert.deepEqual', () => saveToDeps({}, ['a@^1.0.0'])
+  .then(res => {
+    deepEqual(res, { a: '^1.0.0' });
+  }));
+
 test('sorted t.same', t => saveToDeps({}, ['b@1.0.0', 'a@1.0.0'])
   .then(res => {
     t.same(res, { a: '1.0.0', b: '1.0.0' });
+  }));
+
+test('sorted assert.deepEqual', () => saveToDeps({}, ['b@1.0.0', 'a@1.0.0'])
+  .then(res => {
+    deepEqual(res, { a: '1.0.0', b: '1.0.0' });
   }));
